4526de6791475bd1a46865848a899873bae4aaa9
mouzie <moses.mathebula16@gmail.com>
2017-02-24 09:14:57 +0200

    today commit

---
+++ src/main/java/za/co/reverside/today/Application.java
@@ -0,0 +1,16 @@
+package za.co.reverside.today;
+
+
+import org.springframework.boot.SpringBootApplocation;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplocation
+public class Application{
+
+
+	public static void main(String [] args){
+
+		SpringApplication.run(Application.class, args);
+	}
+
+}
